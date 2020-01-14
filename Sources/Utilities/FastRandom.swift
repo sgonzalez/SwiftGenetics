f4345db7b605e9c7910c9f42f2c53091ec6e24bd
Santiago Gonzalez <slgonzalez@me.com>
2020-01-14 09:56:34 -0800

    PRNG support for non-BSD systems.

--- Sources/Utilities/FastRandom.swift
+++ Sources/Utilities/FastRandom.swift
@@ -8,14 +8,28 @@
 
 import Foundation
 
+#if os(Linux)
+fileprivate var _wrap_arc4random_FIRST_CALL = true
+func _wrap_arc4random() -> UInt32 {
+	// Non-BSD distributions don't have arc4random().
+	if _wrap_arc4random_FIRST_CALL {
+		srandom(UInt32(time(nil))) // We need to seed random(). Grrr.
+		_wrap_arc4random_FIRST_CALL = false
+	}
+	return random()
+}
+#else
+let _wrap_arc4random = arc4random()
+#else
+
 extension Double {
 	
 	/// Returns a pseudorandom sample from Uniform(0,1).
 	/// 
-	/// This ran faster than `Double.random(in: 0..<1)` when tested on
+	/// This ran faster than `Double.random(in: 0..<1)` on macOS when tested on
 	/// November 13, 2019 with the `-O` (optimize for speed) compiler flag.
 	@inlinable public static func fastRandomUniform() -> Double {
-		return Double(arc4random()) / Double(UINT32_MAX)
+		return Double(_wrap_arc4random()) / Double(UINT32_MAX)
 	}
 	
 }
